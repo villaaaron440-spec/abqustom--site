5cc1248a43c7133bedb2d8987612109ba49a99df
Aaron Villa <villaaaron440@gmail.com>
2025-11-08 13:52:28 -0600

    Create layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,12 @@
+import type { Metadata } from 'next';
+import './globals.css';
+
+export const metadata: Metadata = { title: 'ABQUSTOM', description: 'American Made On Demand' };
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
